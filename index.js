ab2a11878b7e954fac403bdc93fc679def9798e8
Sydney Mitchell <Sydney@Mac.attlocal.net>
2025-01-09 11:08:07 -0600

    starting framework code

---
+++ index.js
@@ -0,0 +1,49 @@
+const freelancers = [
+  {
+    name: 'Alice',
+    occupation: 'Writer',
+    price: 30,
+  },
+  {
+    name: 'Bob',
+    occupation: 'Teacher',
+    price: 50,
+  },
+]
+
+const possibleFreelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+
+// function that calculates average starting price of freelancers
+function calculateAverageStartingPrice() {
+
+};
+
+// add freelancer to existing array
+function addFreelancer() {
+  // add a bunch of Carols?
+}
+
+// iterate thru freelancers to create an element for each one
+// and add info to the element and add element to DOM
+function renderFreelancer() {
+  // iterates thru array
+
+}
+// function that renders items into the DOM
+function render() {
+  
+}
+
+setInterval(() => {
+  addFreelancer();
+  render();
+}, 1000)
