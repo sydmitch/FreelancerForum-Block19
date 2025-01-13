ab38ae24f326c094c24b0147b7b9d559b070e8ba
Sydney Mitchell <Sydney@Mac.attlocal.net>
2025-01-12 18:42:21 -0600

    Cleaned up syntax, comments, redundant code

--- index.js
+++ index.js
@@ -1,81 +1,77 @@
 // initial freelancers
 const freelancers = [
-  {
-    name: 'Alice',
-    occupation: 'Writer',
-    price: 30,
-  },
-  {
-    name: 'Bob',
-    occupation: 'Teacher',
-    price: 50,
-  },
-]
-
-// possible random freelancers
-const possibleFreelancers = [
-  { name: "Dr. Slice", price: 25, occupation: "gardener" },
-  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-  { name: "Prof. Prism", price: 81, occupation: "teacher" },
-  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-  { name: "Prof. Spark", price: 76, occupation: "programmer" },
-  { name: "Dr. Wire", price: 47, occupation: "teacher" },
-  { name: "Prof. Goose", price: 72, occupation: "driver" },
+  { name: 'Alice', occupation: 'Writer', price: 30 },
+  { name: 'Bob', occupation: 'Teacher', price: 50 },
+  { name: 'Carol', occupation: 'Programmer', price: 70 },
 ];
 
+// possible freelancers
+const possibleFreelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "Gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "Programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "Teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "Teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "Teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "Programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "Teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "Driver" },
+];
 
-// function to calculate average starting price
+// calculate average starting price of freelancers
 function calculateAverageStartingPrice() {
   const totalPrice = freelancers.reduce((sum, freelancer) => sum + freelancer.price, 0);
   return (totalPrice / freelancers.length).toFixed(2);
 };
 
-
-// function to add random freelancer to existing array
-function addFreelancer() {
-  const randomFreelancer = possibleFreelancers[Math.floor(Math.random() * possibleFreelancers.length)];
-  freelancers.push(randomFreelancer);
-}
-
-// iterate thru freelancers to create an element for each one
-// and add info to the element and render element to DOM
+// render freelancers
 function renderFreelancer() {
-  const freelancerContainer = document.getElementById('freelancerContainer');
+  // select container using document.querySelector
+  const freelancerContainer = document.querySelector('#freelancerContainer');
   freelancerContainer.innerHTML = '';
-  // iterates thru array
+ 
   freelancers.forEach((freelancer) => {
     const freelancerDiv = document.createElement('div');
     freelancerDiv.className = 'freelancer';
-    freelancerDiv.innerHTML = `
-      Name: ${freelancer.name}<br>
-      Occupation: ${freelancer.occupation}<br>
-      Starting Price: $${freelancer.price}<br>
-      <br>
-      `;
+
+    const nameElement = document.createElement('div');
+    nameElement.textContent = `Name: ${freelancer.name}`
+    freelancerDiv.appendChild(nameElement);
+
+    const occupationElement = document.createElement('div');
+    occupationElement.textContent = `Occupation: ${freelancer.occupation}`;
+    freelancerDiv.appendChild(occupationElement);
+
+    const priceElement = document.createElement('div');
+    priceElement.textContent = `Starting Price: $${freelancer.price}`;
+    freelancerDiv.appendChild(priceElement);
+
+    // add line break between freelancers
+    const lineBreak = document.createElement('br');
+    freelancerDiv.append(lineBreak);
+
     freelancerContainer.appendChild(freelancerDiv);
   })
 }
 
-// function to update starting price into the DOM
+// render average price
 function renderAveragePrice() {
   const averagePriceElement = document.getElementById('averagePrice');
   averagePriceElement.textContent = calculateAverageStartingPrice();
 }
 
-// function that renders items into the DOM
-// updates the DOM to reflect the current state
+// render all
 function render() {
   renderFreelancer();
   renderAveragePrice();
 }
 
-// intial render to DOM
+// intial render
 render();
 
-// add freelancer to render to page every 5 seconds
+// add freelancer to render to page every 5 seconds w/ updated average price
 setInterval(() => {
-  addFreelancer();
-  render();
+  const newFreelancer = possibleFreelancers[Math.floor(Math.random() * possibleFreelancers.length)];
+  freelancers.push(newFreelancer);
+  renderFreelancer();
+  renderAveragePrice();
 }, 5000)
-
