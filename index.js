adf2a23e0ea7f14cb21cf112a330c0d1c3ac181b
Sydney Mitchell <Sydney@Mac.attlocal.net>
2025-01-12 18:02:46 -0600

    initial code before cleaning up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// initial freelancers
 const freelancers = [
   {
     name: 'Alice',
@@ -11,6 +12,7 @@ const freelancers = [
   },
 ]
 
+// possible random freelancers
 const possibleFreelancers = [
   { name: "Dr. Slice", price: 25, occupation: "gardener" },
   { name: "Dr. Pressure", price: 51, occupation: "programmer" },
@@ -22,28 +24,58 @@ const possibleFreelancers = [
   { name: "Prof. Goose", price: 72, occupation: "driver" },
 ];
 
-// function that calculates average starting price of freelancers
-function calculateAverageStartingPrice() {
 
+// function to calculate average starting price
+function calculateAverageStartingPrice() {
+  const totalPrice = freelancers.reduce((sum, freelancer) => sum + freelancer.price, 0);
+  return (totalPrice / freelancers.length).toFixed(2);
 };
 
-// add freelancer to existing array
+
+// function to add random freelancer to existing array
 function addFreelancer() {
-  // add a bunch of Carols?
+  const randomFreelancer = possibleFreelancers[Math.floor(Math.random() * possibleFreelancers.length)];
+  freelancers.push(randomFreelancer);
 }
 
 // iterate thru freelancers to create an element for each one
-// and add info to the element and add element to DOM
+// and add info to the element and render element to DOM
 function renderFreelancer() {
+  const freelancerContainer = document.getElementById('freelancerContainer');
+  freelancerContainer.innerHTML = '';
   // iterates thru array
-
+  freelancers.forEach((freelancer) => {
+    const freelancerDiv = document.createElement('div');
+    freelancerDiv.className = 'freelancer';
+    freelancerDiv.innerHTML = `
+      Name: ${freelancer.name}<br>
+      Occupation: ${freelancer.occupation}<br>
+      Starting Price: $${freelancer.price}<br>
+      <br>
+      `;
+    freelancerContainer.appendChild(freelancerDiv);
+  })
 }
+
+// function to update starting price into the DOM
+function renderAveragePrice() {
+  const averagePriceElement = document.getElementById('averagePrice');
+  averagePriceElement.textContent = calculateAverageStartingPrice();
+}
+
 // function that renders items into the DOM
+// updates the DOM to reflect the current state
 function render() {
-  
+  renderFreelancer();
+  renderAveragePrice();
 }
 
+// intial render to DOM
+render();
+
+// add freelancer to render to page every 5 seconds
 setInterval(() => {
   addFreelancer();
   render();
-}, 1000)
+}, 5000)
+
